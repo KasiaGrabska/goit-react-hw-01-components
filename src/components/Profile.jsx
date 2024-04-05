bbc3003d9d82a5223ba5953db91762d18cc935ec
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-04-05 16:54:09 +0200

    first task

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,49 +1,43 @@
-import clsx from 'clsx';
 import PropTypes from 'prop-types';
+import styles from '../styles/Profile.module.css';
 
-export const Profile = ({ user }) => {
-  const { username, tag, location, avatar, stats } = user;
+export const Profile = ({ username, tag, location, avatar, stats }) => {
   const { followers, views, likes } = stats;
 
   return (
-    <>
-      <div className={clsx('profile')}>
-        <div className={clsx('description')}>
-          <img src={avatar} alt="User avatar" className={clsx('avatar')} />
-          <p>{username}</p>
-          <p>@{tag}</p>
-          <p>{location}</p>
-        </div>
-
-        <ul className={clsx('stats')}>
-          <li>
-            <span className={clsx('label')}>Followers</span>
-            <span className={clsx('quantity')}>{followers}</span>
-          </li>
-          <li>
-            <span className={clsx('label')}>Views</span>
-            <span className={clsx('quantity')}>{views}</span>
-          </li>
-          <li>
-            <span className={clsx('label')}>Likes</span>
-            <span className={clsx('quantity')}>{likes}</span>
-          </li>
-        </ul>
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt="User avatar" className={styles.avatar} />
+        <p className={styles.name}>{username}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
       </div>
-    </>
+      <ul className={styles.stats}>
+        <li>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
   );
 };
 
 Profile.propTypes = {
-  user: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-    tag: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-    stats: PropTypes.shape({
-      followers: PropTypes.number.isRequired,
-      views: PropTypes.number.isRequired,
-      likes: PropTypes.number.isRequired,
-    }).isRequired,
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
   }).isRequired,
 };
--- src/index.js
+++ src/index.js
@@ -3,13 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import { Profile } from '../src/components/Profile';
-import user from '../src/data/user.json';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App>
-      <Profile user={user} />
-    </App>
+    <App />
   </React.StrictMode>
 );
