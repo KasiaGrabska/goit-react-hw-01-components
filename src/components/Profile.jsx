f3e8138443a3514a84fd488dd738377e73f26ef1
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-04-05 16:55:07 +0200

    changes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,28 +5,30 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
   const { followers, views, likes } = stats;
 
   return (
-    <div className={styles.profile}>
-      <div className={styles.description}>
-        <img src={avatar} alt="User avatar" className={styles.avatar} />
-        <p className={styles.name}>{username}</p>
-        <p className={styles.tag}>@{tag}</p>
-        <p className={styles.location}>{location}</p>
+    <>
+      <div className={styles.profile}>
+        <div className={styles.description}>
+          <img src={avatar} alt="User avatar" className={styles.avatar} />
+          <p className={styles.name}>{username}</p>
+          <p className={styles.tag}>@{tag}</p>
+          <p className={styles.location}>{location}</p>
+        </div>
+        <ul className={styles.stats}>
+          <li>
+            <span className={styles.label}>Followers</span>
+            <span className={styles.quantity}>{followers}</span>
+          </li>
+          <li>
+            <span className={styles.label}>Views</span>
+            <span className={styles.quantity}>{views}</span>
+          </li>
+          <li>
+            <span className={styles.label}>Likes</span>
+            <span className={styles.quantity}>{likes}</span>
+          </li>
+        </ul>
       </div>
-      <ul className={styles.stats}>
-        <li>
-          <span className={styles.label}>Followers</span>
-          <span className={styles.quantity}>{followers}</span>
-        </li>
-        <li>
-          <span className={styles.label}>Views</span>
-          <span className={styles.quantity}>{views}</span>
-        </li>
-        <li>
-          <span className={styles.label}>Likes</span>
-          <span className={styles.quantity}>{likes}</span>
-        </li>
-      </ul>
-    </div>
+    </>
   );
 };
 
