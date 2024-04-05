c7e4fffbfcd3802ef509cd5b4c2e5e6645debbd4
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-04-05 16:37:24 +0200

    import profile

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+import { Profile } from '../src/components/Profile';
+import user from '../src/data/user.json';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App></App>
+    <App>
+      <Profile user={user} />
+    </App>
   </React.StrictMode>
 );
